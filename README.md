ee12c0de883e0465860a6d1763f48643b002eea5
Daniel Tschan <tschan@puzzle.ch>
2024-11-23 17:03:51 +0100

    Add compression support for uploads

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # s3-proxy
-Reverse Proxy signing requests for S3 object store. Allows to up- and download files to/from S3 buckets with curl only.
+Reverse Proxy signing requests for S3 object store. Allows to up- and download files to/from S3 buckets with curl only. Optionally compresses the file while uploading it to S3.
 
 ## Installation on OpenShift
 
@@ -20,8 +20,10 @@ Only the `curl` tool is needed to use this proxy.
 ## Usage
 
 ```sh
-curl -sS -H S3-Host:<S3 host> -H Access-Key:<bucket access-key> -H Secret-Key:<S3 bucket secret key> https://<s3-proxy host>/<S3 bucket path>
+curl -sS -H S3-Host:<S3 host> -H Access-Key:<bucket access-key> -H Secret-Key:<S3 bucket secret key> -H Compress-File:<compression method> https://<s3-proxy host>/<S3 bucket path>
 ```
+
+Currently the only supported compression method is `gzip`. No compression is applied if no `Compress-File` header is set.
 
 ## Example
 
@@ -30,10 +32,10 @@ Upload Java Heap Dumps to S3, especially useful in Kubernetes Pods:
 ```sh
 JAVA_TOOL_OPTIONS:
   -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/dump.hprof
-  -XX:OnOutOfMemoryError="gzip /tmp/dump.hprof &&
-    curl -T /tmp/dump.hprof.gz
+  -XX:OnOutOfMemoryError="curl -T /tmp/dump.hprof
     -H S3-Host:s3-objects.example.org
     -H Access-Key:${S3_ACCESS_KEY}
     -H Secret-Key:${S3_SECRET_KEY}
+    -H Compress-File:gzip
     https://s3-proxy.example.org/java-dumps/dump_$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)_${HOSTNAME}.hprof.gz"
 ```           
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
 use std::error::Error;
-use std::process::exit;
-use std::{convert::Infallible, net::SocketAddr};
+use std::io;
+use std::net::SocketAddr;
 
+use async_compression::tokio::bufread::GzipEncoder;
+use base64::prelude::*;
 use chrono::Utc;
-use futures::stream::{self, StreamExt};
+use futures::stream::StreamExt;
+use futures::TryStreamExt;
 use hmac_sha1::hmac_sha1;
-use http_body_util::{combinators::BoxBody, BodyExt, BodyStream, Empty, Full, StreamBody};
+use http_body_util::{combinators::BoxBody, BodyExt, BodyStream, Full, StreamBody};
 use hyper::{
-    body::{Body, Bytes, Frame, Incoming},
-    header::HeaderValue,
+    body::{Bytes, Frame, Incoming},
     server::conn::http1,
     service::service_fn,
     Request, Response, StatusCode,
 };
-//use hyper::service::{make_service_fn, service_fn};
-//use hyper::{body, Body, Client, Request, Response, Server, StatusCode};
 use hyper_tls::HttpsConnector;
 use hyper_util::{
     client::legacy::Client,
     rt::{TokioExecutor, TokioIo, TokioTimer},
 };
 use tokio::net::TcpListener;
-
-// type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
-
-// resource="/${bucket}/${file}"
-// content_type="application/octet-stream"
-// date=`date -R --utc`
-// _signature="PUT\n\n${content_type}\ nown${date}\n${resource}"
-// signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
-
-// curl -X PUT -T "${file}" \
-//           -H "Host: ${host}" \
-//           -H "Date: ${date}" \
-//           -H "Content-Type: ${content_type}" \
-//           -H "Authorization: AWS ${s3_key}:${signature}" \
-//           https://${host}${resource}
+use tokio_util::io::{ReaderStream, StreamReader};
+use tracing::{error, info, Span};
+use tracing_logfmt;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
     Full::new(chunk.into())
@@ -46,9 +35,6 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
 async fn proxy_handler(
     mut request: Request<Incoming>,
 ) -> Result<Response<Incoming>, Box<dyn Error>> {
-    //let mut res = Response::new(Body::empty());
-
-    //let header_access_key = request.headers_mut().remove("Access-Key");
     let s3_host = request
         .headers_mut()
         .remove("S3-Host")
@@ -70,14 +56,13 @@ async fn proxy_handler(
         .to_str()
         .map_err(|_| "400 Invalid characters in header 'Secret-Key'!")?
         .to_owned();
-    let request_encoding = request
+    let compress_file = request
         .headers_mut()
-        .remove("Request-Encoding")
+        .remove("Compress-File")
         .unwrap_or("".parse()?)
         .to_str()
-        .map_err(|_| "400 Invalid characters in header 'Request-Encoding'!")?
+        .map_err(|_| "400 Invalid characters in header 'Compress-File'!")?
         .to_owned();
-
     let content_type = request
         .headers_mut()
         .entry("content-type")
@@ -85,10 +70,12 @@ async fn proxy_handler(
         .to_str()
         .map_err(|_| "400 Invalid characters in header 'Content-Type'!")?
         .to_owned();
+
     request.headers_mut().insert("host", s3_host.parse()?);
+
     let date = Utc::now().format("%a, %d %b %Y %T %z").to_string();
     request.headers_mut().insert("Date", date.parse()?);
-    //request.headers_mut().insert("Date", HeaderValue::from_str(&date).unwrap());
+
     let signature = format!(
         "{}\n\n{}\n{}\n{}",
         request.method(),
@@ -96,82 +83,116 @@ async fn proxy_handler(
         date,
         request.uri().path()
     );
-    //let mut mac = Hmac::<Sha1>::new_from_slice(secret_key.as_bytes());
-    //mac.update(signature.as_bytes());
     let signature_hash = hmac_sha1(secret_key.as_bytes(), signature.as_bytes());
-    println!("{:?}", signature);
-    println!("{}", base64::encode(signature_hash));
 
-    // let authorization = format!("AWS {}:{}", String::from_utf8_lossy(access_key.as_bytes()), base64::encode(signature_hash));
-    let authorization = format!("AWS {}:{}", access_key, base64::encode(signature_hash));
+    let authorization = format!(
+        "AWS {}:{}",
+        access_key,
+        BASE64_STANDARD.encode(signature_hash)
+    );
     request
         .headers_mut()
         .insert("Authorization", authorization.parse()?);
-    //request.
 
-    // let url = format!("{}{}", String::from_utf8_lossy(s3_host.as_bytes()), request.uri().path());
     let url = format!("https://{}{}", s3_host, request.uri().path());
-    println!("{}", url);
     *request.uri_mut() = url.parse()?;
 
     let https = HttpsConnector::new();
-    let client = Client::builder(TokioExecutor::new()).build::<_, Incoming>(https);
-    //let stream = request.into_parts()
-    //let request = request.body_mut() = Body::wrap_stream(Gz::new(
-    //println!("{:?}", request);
-    let response = client.request(request).await;
-
-    println!("{:?}", response);
+    let response = if request.method() == "PUT" && compress_file == "gzip" {
+        let (mut parts, body) = request.into_parts();
+        parts.headers.remove("Content-Length");
+        let stream = body
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+            .into_data_stream();
+        let stream_reader = StreamReader::new(stream);
+        let encoder = GzipEncoder::new(stream_reader);
+        let stream = ReaderStream::new(encoder).map_ok(Frame::data);
+        let body = StreamBody::new(stream);
+        let request = Request::from_parts(parts, body);
+        let client = Client::builder(TokioExecutor::new()).build(https);
+        client.request(request).await
+    } else {
+        let client = Client::builder(TokioExecutor::new()).build(https);
+        client.request(request).await
+    };
 
     Ok(response?)
-    //Ok(Response::new(Body::from("Hello World!")))
-    //Ok(Response::new(request.into_body()))
 }
 
-// fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-//     Full::new(chunk.into())
-//         .map_err(|never| match never {})
-//         .boxed()
-// }
+fn client_ip(request: &Request<Incoming>, addr: Option<SocketAddr>) -> String {
+    request
+        .headers()
+        .get("x-forwarded-for")
+        .and_then(|h| h.to_str().ok())
+        .map(String::from)
+        .unwrap_or_else(|| {
+            addr.map(|a| a.ip().to_string())
+                .unwrap_or_else(|| "-".to_string())
+        })
+}
+
+/* fn log_request(client_ip: &str,  ) -> Span {
+    let remote_addr = client_ip(request, addr);
+
+    info_span!(
+        "",
+        remote_addr = remote_addr,
+        method = request.method().to_string(),
+        path = request.uri().path(),
+        status = field::Empty,
+        size = field::Empty
+    )
+} */
+
+fn log_response<B>(client_ip: &str, method: &str, path: &str, response: &Response<B>) {
+    let span = Span::current();
+    let status = response.status().as_u16();
+    span.record("status", status);
+    
+    let size = response
+        .headers()
+        .get(hyper::header::CONTENT_LENGTH)
+        .and_then(|length| length.to_str().ok())
+        .unwrap_or("-");
+    span.record("size", size);
+    
+    info!(
+        remote_addr = client_ip,
+        method = method,
+        path = path,
+        status = status,
+        size = size,
+    );
+}
 
 async fn proxy_handler_wrapper(
     request: Request<Incoming>,
+    addr: SocketAddr,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let client_ip = client_ip(&request, Some(addr));
+    let method = request.method().to_string();
+    let path = request.uri().path().to_string();
     match proxy_handler(request).await {
         Ok(response) => {
             let (mut parts, body) = response.into_parts();
             if parts.status.is_client_error() || parts.status.is_server_error() {
-                // println!("{:?}", body);
+                parts.headers.remove("Content-Length");
                 let prefix = "Upstream: ";
                 let suffix = "\r\n";
-                //let (parts, body) = response.into_parts();
-                //let stream = Body::from(prefix).chain(body).chain(Body::from(suffix));
-                let stream = BodyStream::new(full(prefix));
-                //let stream = Full::new(prefix.into()).into_data_stream();
-                //let stream2 = body.into_data_stream();
-                let stream2 = BodyStream::new(body);
-                //let stream3 = Full::new(suffix.into()).into_data_stream();
-                let stream = stream.chain(stream2);
-                //response = Response::from_parts(parts, Body::wrap_stream(stream));
-                let body = BoxBody::new(StreamBody::new(stream));
-
-                // Update Content-Length header to factor in added prefix and suffix
-                if let Some(length) = parts.headers.get("Content-Length") {
-                    let mut length = length.to_str().unwrap_or("").parse::<usize>().unwrap_or(0);
-                    if length > 0 {
-                        length += prefix.len() + suffix.len();
-                        parts
-                            .headers
-                            .insert("Content-Length", HeaderValue::from(length));
-                    }
-                }
-                Ok(Response::from_parts(parts, body))
+                let stream = BodyStream::new(full(prefix))
+                    .chain(BodyStream::new(body))
+                    .chain(BodyStream::new(full(suffix)));
+                let body = StreamBody::new(stream);
+                let response = Response::from_parts(parts, BodyExt::boxed(body));
+                log_response(&client_ip, &method, &path, &response);
+                Ok(response)
             } else {
-                Ok(Response::from_parts(parts, body.boxed()))
+                let response = Response::from_parts(parts, body.boxed());
+                log_response(&client_ip, &method, &path, &response);
+                Ok(response)
             }
         }
         Err(error) => {
-            println!("Error: {}", error);
             let status: StatusCode = error.to_string()[0..3]
                 .parse()
                 .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
@@ -179,6 +200,7 @@ async fn proxy_handler_wrapper(
                 .status(status)
                 .body(full(format!("S3-Proxy: {}\n", error)).boxed())
                 .unwrap();
+            log_response(&client_ip, &method, &path, &response);
             Ok(response)
         }
     }
@@ -186,46 +208,36 @@ async fn proxy_handler_wrapper(
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // For every connection, we must make a `Service` to handle all
-    // incoming HTTP requests on said connection.
-    /*     let make_svc = make_service_fn(|_conn| {
-        // This is the `Service` that will handle the connection.
-        // `service_fn` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(proxy_handler_wrapper)) }
-    }); */
+    // Initialize logging with logfmt format and env-filter
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(tracing_logfmt::Builder::new()
+            .with_target(false)
+            .layer());
+    tracing::subscriber::set_global_default(subscriber)?;
 
     let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
 
-    //let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
-    /*     let mut status: i32 = 0;
-    let graceful = server.with_graceful_shutdown(shutdown_signal(&mut status));
-    if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
-    } */
-    // println!("{:?}", graceful);
-    // server.await?;
+    info!(port = 8080, "Starting server");
 
     let listener = TcpListener::bind(addr).await?;
     loop {
-        let (tcp, _) = listener.accept().await?;
+        let (tcp, addr) = listener.accept().await?;
         let io = TokioIo::new(tcp);
 
         tokio::task::spawn(async move {
-            // Handle the connection from the client using HTTP1 and pass any
-            // HTTP requests received on that connection to the `hello` function
             if let Err(err) = http1::Builder::new()
                 .timer(TokioTimer::new())
-                .serve_connection(io, service_fn(proxy_handler_wrapper))
+                .serve_connection(
+                    io,
+                    service_fn(move |req| proxy_handler_wrapper(req, addr)),
+                )
                 .await
             {
-                println!("Error serving connection: {:?}", err);
+                error!(error = ?err, "Error serving connection");
             }
         });
     }
-    //exit(status);
-    //Ok(())
 }
